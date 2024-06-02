ae21fcb2075cc3f091d4bd146a42129cb0008266
bijoymamud <bijoymamud.09@gmail.com>
2024-06-03 00:09:57 +0600

    menuItems/reviews shown in UI

--- index.js
+++ index.js
@@ -8,6 +8,61 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+//mongodb Connection
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vrgzzke.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+
+    await client.connect();
+
+
+    const menuCollection = client.db("tasteTreasureDB").collection("menu");
+    const reviewsCollection = client.db("tasteTreasureDB").collection("reviews")
+
+
+
+
+    //geting menu items
+    app.get('/menu', async (req, res) => {
+      const result = await menuCollection.find().toArray();
+      res.send(result);
+    })
+
+
+    //geting reviews
+    app.get("/reviews", async (req, res) => {
+      const result = await reviewsCollection.find().toArray();
+      res.send(result);
+    })
+
+
+
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
 app.get('/', (req, res) => {
   res.send("Taste Treasure on Production")
 })
